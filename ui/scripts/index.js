cb368e46474681a7f06e1a144b7095864bb4b238
ultimate <ultimatej@gmx.net>
2017-06-11 14:53:25 +0200

    Added menu

--- ui/scripts/index.js
+++ ui/scripts/index.js
@@ -3,6 +3,8 @@ var UI = function() {
 	this.initialize = function()
 	{
 		this.search = new AutoHide($("#search_textfield"), 5000, "keydown keypressed keyup click mousedown mouseup");
+		
+		this.menu = null;
 	};	
 	
 	return this;
