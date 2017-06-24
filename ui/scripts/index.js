209241fecb0ae03d0285985b5f64813e729a3624
ultimate <ultimatej@gmx.net>
2017-06-24 22:00:35 +0200

    added "manage categories" window

--- ui/scripts/index.js
+++ ui/scripts/index.js
@@ -1,6 +1,6 @@
 var UI = function() {
 	
-	this.populateMenu = function()
+	this.populateCategories = function()
 	{
 		$("#menu_categories li").remove();
 		
@@ -10,6 +10,7 @@ var UI = function() {
 		for(var c in categories)
 		{
 			console.log(categories[c]);
+			// populate menu
 			if(categories[c].favorite)
 			{
 				element = $("\
@@ -35,6 +36,56 @@ var UI = function() {
 				}(this, c, element));
 				$("#menu_categories_list").append(element);
 			}
+			
+			// populate manage categories	
+			{
+				element = $("\
+					<li class='draggable selectable'><label key='" + categories[c].key + "'/>\
+						<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='1em' height='1em' viewBox='0 0 100 100' class='image star favorite'>\
+							<use xlink:href='#img_star_active'/>\
+						</svg>\
+						<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='1em' height='1em' viewBox='0 0 100 100' class='image star no_favorite'>\
+							<use xlink:href='#img_star_inactive'/>\
+						</svg>\
+						<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='1em' height='1em' viewBox='0 0 100 100' class='image up'>\
+							<use xlink:href='#img_arrow_up'/>\
+						</svg>\
+						<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='1em' height='1em' viewBox='0 0 100 100' class='image down'>\
+							<use xlink:href='#img_arrow_down'/>\
+						</svg>\
+					</li>");
+				if(categories[c].favorite)
+					element.addClass("favorite");
+				element.children(".star").click(function(ui, i, e) {
+					return function(event) {
+						console.log("click star");
+						ui.categories[i].favorite = !ui.categories[i].favorite;						
+						if(ui.categories[i].favorite)
+							e.addClass("favorite");
+						else
+							e.removeClass("favorite");
+						app.setCategories(ui.categories);
+					};
+				}(this, c, element));
+				element.children(".up").click(function(ui, i, e) {
+					return function(event) {
+						console.log("click up");
+						if(e.prev().length > 0)
+							e.insertBefore(e.prev());
+						app.setCategories(ui.categories);
+					};
+				}(this, c, element));
+				element.children(".down").click(function(ui, i, e) {
+					return function(event) {
+						console.log("click down");
+						if(e.next().length > 0)
+							e.insertAfter(e.next());
+						app.setCategories(ui.categories);
+					};
+				}(this, c, element));
+				
+				$("#manage_categories_list").append(element);
+			}
 		}
 	};
 	
@@ -48,7 +99,7 @@ var UI = function() {
 		$("#main").click(function(event) { if(!event.isPropagationStopped()) UI.menu.hide(); });
 		
 		/* initialize menu */
-		this.populateMenu();
+		this.populateCategories();
 		$("menu_categories").click(function(event) {});
 		$("menu_profiles").click(function(event) {});
 		$("menu_settings").click(function(event) {});
