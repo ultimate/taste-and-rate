e97d00088a1f7fe35df6cfc11d19c36e55ecd245
ultimate <ultimatej@gmx.net>
2017-07-29 17:12:16 +0200

    removed JQuery #3

--- ui/scripts/util/labels.js
+++ ui/scripts/util/labels.js
@@ -16,24 +16,30 @@ var LabelManager = function(labelProvider, labelFunction) {
 	this.updateLabels = function(parent)
 	{
 		if(parent == undefined)
+			parent = document;		
+		else if(parent.getElementsByTagName == undefined)
 			parent = document;
-	
-		var elements = $(parent).find("label");
-		
-		elements.each(function(lm) {
-			return function(index, element) {
-				console.log("updating label key='" + element.getAttribute(lm.KEY_ATTRIBUTE) + "'");
-				// remove text nodes
-				$(element).contents().filter(function() {
-					return this.nodeType == 3; //Node.TEXT_NODE
-				}).remove();
-				// append new text
-				$(element).append( $(document.createTextNode(lm.getString(element.getAttribute(lm.KEY_ATTRIBUTE)))) );
-			};
-		}(this) );
+		var elements;
+		// labels
+		elements = parent.getElementsByTagName(this.LABEL_TAGNAME);
+		for( var i = 0; i < elements.length; i++)
+		{
+			console.log("updating label key='" + elements[i].getAttribute(this.KEY_ATTRIBUTE) + "'");
+			// remove all current children
+			while(elements[i].firstChild)
+				elements[i].removeChild(elements[i].firstChild);
+			elements[i].appendChild(document.createTextNode(this.getString(elements[i].getAttribute(this.KEY_ATTRIBUTE))));
+		}
+		// buttons
+		elements = document.getElementsByTagName(this.BUTTON_TAGNAME);
+		for( var i = 0; i < elements.length; i++)
+		{
+			if(elements[i].getAttribute("type") == "button")
+				elements[i].value = this.getString(elements[i].getAttribute(this.KEY_ATTRIBUTE));
+		}
 		// ToDo other element types like
-		// - buttons (set value instead of content)
 		// - selects
 		// - iframe (reload in different language)
+		//document.getElementById(this.constants.STATIC_FRAME_ID).contentWindow.location.reload(true)
 	};
 };
