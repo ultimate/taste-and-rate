66834b8469313134b1601eb617858d035eb91872
ultimate <ultimatej@gmx.net>
2018-09-05 13:37:57 +0200

    removed debug outputs

--- ui/scripts/util/calendar.js
+++ ui/scripts/util/calendar.js
@@ -146,19 +146,17 @@ var Calendar = function(parent, showNav, firstDayOfWeek, events) {
 					if(this.events[i].date.getFullYear() > this.currentYear || this.events[i].date.getMonth() > this.currentMonth)
 					{
 						index += daysInThisMonth;
-						console.log("increment");
 					}
 					else if(this.events[i].date.getFullYear() < this.currentYear || (this.events[i].date.getMonth() < this.currentMonth && this.events[i].date.getFullYear() == this.currentYear))
 					{
 						index -= daysInPrevMonth;
-						console.log("decrement");
 					}
 					else
 					{
 						console.err("how did we get here: " + this.events[i].date);
 					}
 				}
-				console.log("displaying event: '" + this.events[i].title + "' (" + this.events[i].date + ") @ " + index + " (offset=" + offset + ")");
+				console.log("displaying event: '" + this.events[i].title + "' (" + this.events[i].date + ") @ " + index);
 				element = Elements.fromString("<div class='calendar_event'>\
 												<span class='title'>" + this.events[i].title + "</span>\
 												<span class='location'>" + this.events[i].location + "</span>\
